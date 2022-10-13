5b21e0b7b358744b37fb2f845a5187cfe1968a4b
Jwell Lalakiya <60837250+jrlalakiya@users.noreply.github.com>
2022-10-14 03:00:04 +0530

    Update caesar cipher.java
    i upadted it

--- caesar cipher/caesar cipher.java
+++ caesar cipher/caesar cipher.java
@@ -14,14 +14,20 @@ class CaesarCipher
                                   s - 65) % 26 + 65);
                 result.append(ch);
             }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                                  s - 97) % 26 + 97);
-                result.append(ch);
-            }
         }
         return result;
     }
-
+ 
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Plain Test: ");  
+        String text = sc.nextLine();
+        System.out.print("Enter Key: ");  
+        int s = sc.nextInt(); 
+        System.out.println("\n");
+        System.out.println("Text  : " + text);
+        System.out.println("Shift : " + s);
+        System.out.println("Cipher: " + encrypt(text, s));
+    }
 }
